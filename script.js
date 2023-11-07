573d19059b3a584ff60f2c37bd640462c158363c
coconutco7 <149829864+coconutco7@users.noreply.github.com>
2023-11-07 15:12:43 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+
+// api varibles
+const apiKey = "e59fc793a428f80ace4415ca7c2b14a2";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=";
+
+
+// search varible 
+searchbox = document.querySelector(".search input");
+searchBtn= document.querySelector(".search button");
+// image varible
+weatherIcon = document.querySelector(".weather-icon")
+
+
+
+// function to link to api site 
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    //console.log(data);
+
+if(response.status == 404){
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+}
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°f";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + "m/h";
+    
+if(data.weather[0].main == "clouds"){
+    weatherIcon.src = "images/cloud.png";
+}
+else if(data.weather[0].main == "clear"){
+    weatherIcon.src = "images/clear.png.png";
+}
+else if(data.weather[0].main == "rain"){
+    weatherIcon.src = "images/rain.png.";
+}
+else if(data.weather[0].main == "drizzle"){
+    weatherIcon.src = "images/drizzle.png.";
+}
+else if(data.weather[0].main == "mist"){
+    weatherIcon.src = "images/mist.png";
+}
+else if(data.weather[0].main == "clear"){
+    weatherIcon.src = "images/snow.png";
+}
+
+}
+
+searchBtn.addEventListener ("click", ()=>{
+    CheckWeather(searchBox.value);
+})
+checkWeather();
